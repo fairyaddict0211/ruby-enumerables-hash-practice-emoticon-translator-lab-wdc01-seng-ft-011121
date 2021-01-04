24a9c5afa8831a7b91371fc4337340e35b199463
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-04 22:38:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require "yaml"
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
-new_hash = {}
+  new_hash = {}
 
   emoticons.each do |key,value|
     new_hash[key] = {}
@@ -11,7 +11,6 @@ new_hash = {}
   end
 
   new_hash
-end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
